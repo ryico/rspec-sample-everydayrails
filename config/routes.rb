7cd6072d6b92816b40ebe98a8e9fd6604380126b
ryichk <ryichk@gmail.com>
2021-06-25 04:42:47 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,29 @@ Rails.application.routes.draw do
     :sessions => 'users/sessions'
   }
 
+  authenticated :user do
+    root 'projects#index', as: :authenticated_root
+  end
+
   devise_scope :user do
     get "user/id", :to => "users/registrations#detail"
     get "signup", :to => "users/registrations#new"
     get "login", :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :projects do
+    resources :notes
+    resources :tasks do
+      member do
+        post :toggle
+      end
+    end
+  end
+
+  namespace :api do
+    resources :projects
+  end
+
+  root 'home#index'
 end
