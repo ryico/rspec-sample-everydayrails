b68bdea749013d383bbf6d24bb56d8e86cbac9e1
ryichk <ryichk@gmail.com>
2021-06-25 23:34:48 +0900

    add custom matcher have_content_type

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,6 +33,19 @@ class TasksController < ApplicationController
 
   def update
     respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @project, notice: 'Task was successfully updated.' }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @task.destroy
+    respond_to do |format|
       format.html { redirect_to @project, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
     end
